80ec45a9425e1f31ee5e90b1f71167c7a1794547
Tammy Thai <tammythai94@gmail.com>
2026-01-29 06:16:14 +0000

    Setting up terraform with Github actions

---
+++ 01-docker-terraform/terraform/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "project-378b4ffb-57b4-4f0b-91f"
+  region  = "us-central1"
+
+}
+
+resource "google_storage_bucket" "test-bucket" {
+  name     = "project-378b4ffb-57b4-4f0b-91f-terra-bucket"
+  location = "US"
+
+  # Optional, but recommended settings:
+  storage_class               = "STANDARD"
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 30 // days
+    }
+  }
+
+  force_destroy = true
+}
