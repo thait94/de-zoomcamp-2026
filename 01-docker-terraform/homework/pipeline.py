d016862b352f61714d943c891af4431a73ad3140
Tammy Thai <tammythai94@gmail.com>
2026-01-30 09:39:25 +0000

    Read csv and parquet files and add to Postgresql db

--- 01-docker-terraform/homework/pipeline.py
+++ 01-docker-terraform/homework/pipeline.py
@@ -1,6 +1,8 @@
-# grab read from the data files (parquet and csv)
-# take that data and load it into postgresql db 
-# use pgadmin to run queries 
+# 1. Read from the data files (parquet and csv)
+# 2. Take that data and load it into postgresql db 
+# 3. Use pgadmin to run queries 
+
+# Note: Make sure to run docker-compose file to create volumes and then run the Dockerfile to reference this pipeline
 
 import pandas as pd
 from sqlalchemy import create_engine
@@ -33,29 +35,46 @@ parse_greentrip_dates = [
     "lpep_dropoff_datetime"
 ]
 
-taxizone_lookup_dtypes = {
+taxizone_lookup_dtype = {
     "LocationID": "Int64", 
     "Borough": "string", 
     "Zone": "string", 
     "service_zone": "string"
 }
 
-url = './data/green_tripdata_2025-11.parquet'
-taxizone_lookup_df = pd.read_csv('./data/taxi_zone_lookup.csv')
-
 def ingest_data():
+    location = './data/green_tripdata_2025-11.parquet'
     pg_user = 'root'
     pg_pass = 'root'
     pg_host = 'localhost'
     pg_port = 5432
     pg_db = 'ny_taxi'
-    table_name = 'green_taxi_data'
+    taxi_table = 'green_taxi_data'
+    lookup_table = 'taxi_zone_lookup'
     chunksize = 100000
 
     engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
 
+    # Create a dataframe for taxi zone lookup data
+    taxizone_lookup_df = pd.read_csv('./data/taxi_zone_lookup.csv', header=0, dtype=taxizone_lookup_dtype, engine=engine)
+
+    # Load the header to the db 
+    taxizone_lookup_df.head(0).to_sql(
+        name=lookup_table, 
+        con=engine, 
+        if_exists="replace"
+    )
+
+    # Load the rest of the data to the db 
+    taxizone_lookup_df.to_sql(
+        name=lookup_table, 
+        con=engine, 
+        if_exists="append"
+    )
+
+    # Create an iterator for green trip data
     greentrip_df_iter = pd.read_parquet(
-        url, 
+        location, 
         dtype=greentrip_dtype,
         parse_dates=parse_greentrip_dates, 
         iterator=True, 
@@ -64,12 +83,13 @@ def ingest_data():
 
     first = True
 
-    for df_chunk in tqdm(greentrip_df_iter):
+    # Loop through iterator and add data to db 
+    for greentrip_chunk in tqdm(greentrip_df_iter):
 
         if first:
             # Create table schema (no data)
-            df_chunk.head(0).to_sql(
-                name=table_name,
+            greentrip_chunk.head(0).to_sql(
+                name=taxi_table,
                 con=engine,
                 if_exists="replace"
             )
@@ -77,11 +97,14 @@ def ingest_data():
             print("Table created")
 
         # Insert chunk
-        df_chunk.to_sql(
-            name=table_name,
+        greentrip_chunk.to_sql(
+            name=taxi_table,
             con=engine,
             if_exists="append"
         )
+
+if __name__ == '__main__':
+    ingest_data()
 
 
 
